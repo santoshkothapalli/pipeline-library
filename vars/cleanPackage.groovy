aa1698a16fe0e67c43a556a5859642096f2825b3
santoshkothapalli <santoshkothapalli@gmail.com>
2020-01-04 21:21:44 -0500

    maven clean

--- vars/cleanPackage.groovy
+++ vars/cleanPackage.groovy
@@ -1,4 +1,4 @@
 def call(String command='sh') {
     echo "INFO: Executing the maven clean with ${command}"
-    "${command} mvn -Dmaven.test.failure.ignore=true clean package"
+    ${command} "mvn -Dmaven.test.failure.ignore=true clean package"
 }
